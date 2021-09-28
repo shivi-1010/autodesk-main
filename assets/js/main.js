70652a613b8a67b18679d036da3e550381173977
shivi-1010 <varushivani@gmail.com>
2021-09-28 20:14:12 +0530

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -359,4 +359,10 @@ $("#share-wa").hide();
   
 });
 
-
+$(document).ready(function(){
+  $('.dropdown-submenu a.test').on("click", function(e){
+    $(this).next('ul').toggle();
+    e.stopPropagation();
+    e.preventDefault();
+  });
+});
